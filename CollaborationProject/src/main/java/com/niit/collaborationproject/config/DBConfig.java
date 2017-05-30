5ddaa596e677235442d067eae731913af620269d
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2017-05-30 17:16:53 +0530

    Modified database to H2 from Oracle due to efficiency purposes.

--- CollaborationProject/src/main/java/com/niit/collaborationproject/config/DBConfig.java
+++ CollaborationProject/src/main/java/com/niit/collaborationproject/config/DBConfig.java
@@ -17,7 +17,50 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DBConfig {
 
+	@Bean(name = "dataSource")
+	public DataSource getH2DataSource() {
 
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/collaborationproject");
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("sa");
+
+		return dataSource;
+	}
+	
+	private Properties getHibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		
+		return properties;
+	}
+	
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+
+		LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+		sessionBuilder.addProperties(getHibernateProperties());
+		
+		sessionBuilder.scanPackages("com.niit.collaborationproject");
+
+		return sessionBuilder.buildSessionFactory();
+	}
+	
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+
+		return transactionManager;
+	}
+	
+	
+/*
 	@Bean(name = "dataSource")
 	public DataSource getH2DataSource() {
 
@@ -60,7 +103,7 @@ public class DBConfig {
 		return transactionManager;
 	}
 
-	
+	*/
 	
 	/*@Bean(name="sessionFactory")
 	public SessionFactory getSessionFactory()
